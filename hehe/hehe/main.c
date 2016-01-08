8915428144687650ca9b4ffdda5af7f8bc051674
yuanhaohua <yuanhaohua123@qq.com>
2016-01-08 16:51:36 +0800

    我是气质

---
+++ hehe/hehe/main.c
@@ -0,0 +1,16 @@
+//
+//  main.c
+//  hehe
+//
+//  Created by yuan on 16/1/8.
+//  Copyright © 2016年 yuan. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    printf("最爱大米饭");
+    return 0;
+}
